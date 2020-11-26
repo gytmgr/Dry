40eff9593022531e767ee628fff3f238b8f57908
gan <gytmgr@qq.com>
2020-11-26 15:22:09 +0800

    增加排序dto

---
+++ Application/Dry.Application.Contracts/Dtos/SortDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dry.Application.Contracts.Dtos
+{
+    /// <summary>
+    /// 排序
+    /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
+    public class SortDto<TEnum> where TEnum : Enum
+    {
+        /// <summary>
+        /// 顺序，true为正序，false为倒序
+        /// </summary>
+        public bool Order { get; set; }
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public TEnum Field { get; set; }
+    }
+}
