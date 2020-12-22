6b00799860e6d5a90ea38e1f849254a882296b43
gan <gytmgr@qq.com>
2020-12-22 17:41:03 +0800

    新增基于jwt认证的全局http头参数

--- src/Dry.Swagger/SwaggerExtensions.cs
+++ src/Dry.Swagger/SwaggerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -13,34 +15,78 @@ namespace Dry.Swagger
     public static class SwaggerExtensions
     {
         /// <summary>
+        /// 基础配置
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="title"></param>
+        private static void BasicConfig(SwaggerGenOptions options, string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                title = "Dry API";
+            }
+
+            options.CustomSchemaIds(type => type.ToString());
+
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = title,
+                Version = "v1",
+            });
+
+            Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.xml").ToList().ForEach(file =>
+            {
+                options.IncludeXmlComments(file, true);
+            });
+        }
+
+        /// <summary>
         /// 添加Swagger服务注册
         /// </summary>
         /// <param name="services"></param>
-        /// <param name="assembly"></param>
+        /// <param name="title"></param>
         /// <returns></returns>
         public static IServiceCollection AddCustomSwagger(this IServiceCollection services, string title = null)
         {
-            if (title == null)
-            {
-                title = "Blue API";
-            }
-            services.AddSwaggerGen(cfg =>
-            {
-                cfg.CustomSchemaIds(type => type.ToString());
+            services.AddSwaggerGen(x => BasicConfig(x, title));
+            return services;
+        }
 
-                cfg.SwaggerDoc("v1", new OpenApiInfo
+        /// <summary>
+        /// 添加基于jwt认证的Swagger服务注册
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddJwtSwagger(this IServiceCollection services, string title = null)
+        {
+            services.AddSwaggerGen(x =>
+            {
+                BasicConfig(x, title);
+
+                x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Title = title,
-                    Version = "v1",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Description = "请输入Token",
+                    BearerFormat = "JWT"
                 });
-
-                Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.xml").ToList().ForEach(file =>
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
-                    cfg.IncludeXmlComments(file, true);
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                        },
+                        new List<string>()
+                    }
                 });
             });
             return services;
         }
+
         /// <summary>
         /// 使用Swagger
         /// </summary>
